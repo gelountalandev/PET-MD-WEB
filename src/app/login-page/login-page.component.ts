4be741cfd15675ae4e94104c47065d34bc84e811
Angelo untalan <gelowspotify@gmail.com>
2025-05-02 15:35:07 +0800

    implemented login functionality wherein it shows alert in the page

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+  username = '';
+  password = '';
+
+  onSubmit(): void{
+    console.log('username', this.username);
+    console.log('password', this.password);
+    alert(`Welcome ${this.username}!`);
+  }
+
+}
