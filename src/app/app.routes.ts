2f7f15d165a501d99f2daaff1f26f013adba679d
Angelo untalan <gelowspotify@gmail.com>
2025-05-05 10:15:29 +0800

    changed the component name to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: LoginPageComponent},
