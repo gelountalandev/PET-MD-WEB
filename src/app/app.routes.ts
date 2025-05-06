c2c53729ffa827604932ff86cbd40951d5c2472e
Angelo untalan <gelowspotify@gmail.com>
2025-05-06 16:09:08 +0800

    added PetOwnerRegisterComponent for routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PetOwnerRegisterComponent } from './pet-owner-register/pet-owner-register.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
+    {path: 'register', component: PetOwnerRegisterComponent}
 ];
