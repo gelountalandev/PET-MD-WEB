ae9ef94c96bc9ea1ba83c03cc56bd6500d5b7b23
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-06-02 15:46:05 +0800

    Added constants for models and base_url

---
+++ src/app/app.constants.ts
@@ -0,0 +1,18 @@
+export const BASE_URL = 'http://127.0.0.1:3000/';
+
+export enum USER_TYPE {
+  VET = 'VET',
+  PET_OWNER = 'PET_OWNER',
+}
+
+export enum CONSULTATION_TYPE {
+  ON_GOING = 'ON_GOING',
+  COMPLETED = 'COMPLETED',
+  CANCELLED = 'CANCELLED',
+}
+
+export enum APPOINTMENT_STATUS {
+  VIDEO = 'VIDEO',
+  CHAT = 'CHAT',
+  VOICE = 'VOICE',
+}
